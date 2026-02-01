cb82a06f26122a8ce2002327fffe18bb8c146f1a
Felipe Braga <felipe7braga@gmail.com>
2026-02-01 10:14:30 -0300

    Inicio da database H2, JPA e conta de test

--- src/main/java/com/Projeto/InicioComSpring/Entities/User.java
+++ src/main/java/com/Projeto/InicioComSpring/Entities/User.java
@@ -1,10 +1,17 @@
 package com.Projeto.InicioComSpring.Entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name  = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long Id;
     private String name;
     private String email;
