d36e8ecdad937cffdb1737bbb64b6923a5d6a44f
Felipe Braga <felipe7braga@gmail.com>
2026-02-01 09:39:18 -0300

    Criacao do projeto

---
+++ src/main/java/com/Projeto/InicioComSpring/InicioComSpringApplication.java
@@ -0,0 +1,13 @@
+package com.Projeto.InicioComSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InicioComSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InicioComSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Projeto/InicioComSpring/InicioComSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Projeto.InicioComSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InicioComSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
